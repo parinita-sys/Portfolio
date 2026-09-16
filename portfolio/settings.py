9546f961ff8853bd28637cbf08e004124e70c7a3
parinita-sys <ps0829@srmist.edu.in>
2026-09-16 06:37:12 +0530

    Prepare portfolio for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nl6yhfm_5f%-z5a9xqty3l@1tgt&5hq&+)8%@fd+^!^d+6ylv!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'main/static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
